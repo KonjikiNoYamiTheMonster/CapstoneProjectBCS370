edc9cd8e2f7cdc944743bfa508d645ad75cc7392
KonjikiNoYamiTheMonster <74839004+KonjikiNoYamiTheMonster@users.noreply.github.com>
2020-12-14 21:13:48 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// Authors : Meerza & Joseph
+// Goal : To build homework managment system 
+// BCS 370 Capstone Project
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+#include "hwManagment.h"
+
+int main()
+{
+	Capstone cap;
+	cap.startProject();
+	return 0;
+}
